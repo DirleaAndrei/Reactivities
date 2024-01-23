92ec0b876dfaf473b907b52f64b2e7b3a61cd19c
DirleaAndrei <78161390+DirleaAndrei@users.noreply.github.com>
2024-01-23 11:46:15 +0200

    Pre-deploy

--- Persistence/Migrations/20240122084658_PostgresInitial.Designer.cs
+++ Persistence/Migrations/20240122084658_PostgresInitial.Designer.cs
@@ -12,7 +12,7 @@ using Persistence;
 namespace Persistence.Migrations
 {
     [DbContext(typeof(DataContext))]
-    [Migration("20240104201316_PostgresInitial")]
+    [Migration("20240122084658_PostgresInitial")]
     partial class PostgresInitial
     {
         /// <inheritdoc />
