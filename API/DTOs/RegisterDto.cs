5abebda296d94643b8f44562e7fdcf8c4704274d
DirleaAndrei <78161390+DirleaAndrei@users.noreply.github.com>
2024-01-25 17:26:14 +0200

    Fixed password error messages

--- API/DTOs/RegisterDto.cs
+++ API/DTOs/RegisterDto.cs
@@ -8,7 +8,9 @@ namespace API.DTOs
         [EmailAddress]
         public string Email { get; set; }
         [Required]
-        [RegularExpression("(?=.*\\d)(?=.*[a-z])(?=.*[A-Z]).{4,8}$", ErrorMessage = "Password must be complex!")]
+        [MinLength(4, ErrorMessage = "Minimum number of characters is 4!")]
+        [MaxLength(20, ErrorMessage = "Maximum number of characters is 20!")]
+        [RegularExpression("(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])$", ErrorMessage = "Password must be complex!")]        
         public string Password { get; set; }
         [Required]
         public string DisplayName { get; set; }
